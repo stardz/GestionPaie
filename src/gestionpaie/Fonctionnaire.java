6a1b11080756a929e7470cb1ca9084fd6941e9a5
Oop <Oop@Oo.connectify>
2014-08-18 11:32:58 +0200

    Merge origin/master
    Conflicts:
    	src/gestionpaie/Fonctionnaire.java

--- src/gestionpaie/Fonctionnaire.java
+++ src/gestionpaie/Fonctionnaire.java
@@ -6,11 +6,43 @@
 
 package gestionpaie;
 
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.LongProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleLongProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
 /**
  *
  * @author Oop
  */
 public class Fonctionnaire {
-    private String nom;
-    int NSS ;
+    private IntegerProperty nss;
+    private StringProperty nomFonctionnaire;
+    private StringProperty prenomFonctionnaire;
+    private StringProperty sexe;
+    private StringProperty situationConjoint;
+    private IntegerProperty enfantCharg;
+    private IntegerProperty enfantScolarise;
+    private IntegerProperty enfantPlusDixAns;
+    private LongProperty numMutuelle;
+    private LongProperty numCompte;
+
+    public Fonctionnaire(Integer nss, String nomFonctionnaire, String prenomFonctionnaire, String sexe, String situationConjoint, Integer enfantCharg, Integer enfantScolarise, Integer enfantPlusDixAns, Long numMutuelle, Long numCompte) {
+        this.nss = new SimpleIntegerProperty(nss);
+        this.nomFonctionnaire = new SimpleStringProperty(nomFonctionnaire);
+        this.prenomFonctionnaire = new SimpleStringProperty(prenomFonctionnaire);
+        this.sexe =new SimpleStringProperty(sexe) ;
+        this.situationConjoint =new SimpleStringProperty(situationConjoint) ;
+        
+        this.enfantCharg = new SimpleIntegerProperty(enfantCharg);
+        this.enfantScolarise = new SimpleIntegerProperty(enfantScolarise);
+        this.enfantPlusDixAns = new SimpleIntegerProperty(enfantPlusDixAns);
+        this.numMutuelle = new SimpleLongProperty(numMutuelle);
+        this.numCompte =new SimpleLongProperty(numCompte) ;
+    }
+    
+    
+    
 }
