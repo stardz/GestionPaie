236c644812082fa81bbe3d352a5035c87627d75d
Oop <Oop@Oo.connectify>
2014-08-18 12:31:04 +0200

    Merge origin/master
    Conflicts:
    	src/gestionpaie/Fonctionnaire.java

--- src/gestionpaie/Fonctionnaire.java
+++ src/gestionpaie/Fonctionnaire.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package gestionpaie;
 
 import javafx.beans.property.IntegerProperty;
@@ -18,6 +17,7 @@ import javafx.beans.property.StringProperty;
  * @author Oop
  */
 public class Fonctionnaire {
+
     private IntegerProperty nss;
     private StringProperty nomFonctionnaire;
     private StringProperty prenomFonctionnaire;
@@ -33,16 +33,95 @@ public class Fonctionnaire {
         this.nss = new SimpleIntegerProperty(nss);
         this.nomFonctionnaire = new SimpleStringProperty(nomFonctionnaire);
         this.prenomFonctionnaire = new SimpleStringProperty(prenomFonctionnaire);
-        this.sexe =new SimpleStringProperty(sexe) ;
-        this.situationConjoint =new SimpleStringProperty(situationConjoint) ;
-        
+        this.sexe = new SimpleStringProperty(sexe);
+        this.situationConjoint = new SimpleStringProperty(situationConjoint);
+
         this.enfantCharg = new SimpleIntegerProperty(enfantCharg);
         this.enfantScolarise = new SimpleIntegerProperty(enfantScolarise);
         this.enfantPlusDixAns = new SimpleIntegerProperty(enfantPlusDixAns);
         this.numMutuelle = new SimpleLongProperty(numMutuelle);
-        this.numCompte =new SimpleLongProperty(numCompte) ;
+        this.numCompte = new SimpleLongProperty(numCompte);
     }
-    
-    
+
+    public void setNss(Integer nss) {
+        this.nss = new SimpleIntegerProperty(nss);
+    }
+
+    public void setNomFonctionnaire(String nomFonctionnaire) {
+        this.nomFonctionnaire = new SimpleStringProperty(nomFonctionnaire);
+    }
+
+    public void setPrenomFonctionnaire(String prenomFonctionnaire) {
+        this.prenomFonctionnaire = new SimpleStringProperty(prenomFonctionnaire);
+    }
+
+    public void setSexe(String sexe) {
+        this.sexe = new SimpleStringProperty(sexe);
+    }
+
+    public void setSituationConjoint(String situationConjoint) {
+        this.situationConjoint = new SimpleStringProperty(situationConjoint);
+    }
+
+    public void setEnfantCharg(Integer enfantCharg) {
+        this.enfantCharg = new SimpleIntegerProperty(enfantCharg);
+    }
+
+    public void setEnfantScolarise(Integer enfantScolarise) {
+        this.enfantScolarise =new SimpleIntegerProperty(enfantScolarise) ;
+    }
+
+    public void setEnfantPlusDixAns(Integer enfantPlusDixAns) {
+        this.enfantPlusDixAns = new SimpleIntegerProperty(enfantPlusDixAns);
+    }
+
+    public void setNumMutuelle(Long numMutuelle) {
+        this.numMutuelle = new SimpleLongProperty(numMutuelle);
+    }
+
+    public void setNumCompte(Long numCompte) {
+        this.numCompte = new SimpleLongProperty(numCompte);
+    }
+
+    public Integer getNss() {
+        return nss.getValue();
+    }
+
+    public String getNomFonctionnaire() {
+        return nomFonctionnaire.getValue();
+    }
+
+    public String getPrenomFonctionnaire() {
+        return prenomFonctionnaire.getValue();
+    }
+
+    public String getSexe() {
+        return sexe.getValue();
+    }
+
+    public String getSituationConjoint() {
+        return situationConjoint.getValue();
+    }
+
+    public Integer getEnfantCharg() {
+        return enfantCharg.getValue();
+    }
+
+    public Integer getEnfantScolarise() {
+        return enfantScolarise.getValue();
+    }
+
+    public Integer getEnfantPlusDixAns() {
+        return enfantPlusDixAns.getValue();
+    }
+
+    public Long getNumMutuelle() {
+        return numMutuelle.getValue();
+    }
+
+    public Long getNumCompte() {
+        return numCompte.getValue();
+    }
+ 
     
 }
