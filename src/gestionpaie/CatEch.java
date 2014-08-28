ce34f8a8b5326222e21034f57ebd263464588729
AhmedLattali <ba_lattali@esi.dz>
2014-08-28 11:38:54 +0100

    Merge origin/master
    Conflicts:
    	src/gestionpaie/GestionPaie.java

---
+++ src/gestionpaie/CatEch.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package gestionpaie;
+
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+
+/**
+ *
+ * @author Ahmed
+ */
+public class CatEch {
+    
+    IntegerProperty cat ; 
+    
+    IntegerProperty ech ;
+    
+    public CatEch(){
+
+                    
+    }
+    
+}
